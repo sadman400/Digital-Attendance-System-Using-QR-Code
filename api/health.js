38680cca1099e89f59f4780c5dcfe0e0506641bb
sadman400 <sadmanhossain400@gmail.com>
2025-12-18 05:38:05 +0600

    Force redeploy with version bump

--- api/health.js
+++ api/health.js
@@ -1,4 +1,4 @@
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  return res.status(200).json({ status: 'ok', message: 'API is running' });
+  return res.status(200).json({ status: 'ok', message: 'API is running', version: '2.0' });
 };
